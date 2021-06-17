b757b63eaf83a7faa3b889c8a3704ab28141b845
Josh Wintraub <joshbw4@gmail.com>
2021-06-17 14:24:22 -0400

    Fixed question destroy relation with templates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   has_many :template_questions
-  has_many :templates, through: :template_questions
+  has_many :templates, through: :template_questions, dependent: :destroy # To remove question from many-many table after deleting a questions that was in templates
   validates :ask, presence: true, length: {minimum: 3, maximum: 100}
 end
